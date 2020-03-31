9390eb338bd37123f7a4fb19898869daa23043ed
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2020-03-31 08:32:39 -0600

    added files from yesterday

---
+++ src/main/java/com/abccorp/Runner.java
@@ -0,0 +1,11 @@
+package com.abccorp;
+
+import com.xyzcorp.Person;
+
+class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Person person = new Person("James", "Gosling");
+        System.out.println(person);
+    }
+}
